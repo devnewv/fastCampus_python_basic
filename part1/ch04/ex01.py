28d130955dc93953669b95c25fb32a6b6117a561
devnewv <devnewv@gmail.com>
2023-03-09 00:14:00 +0900

    first commit

---
+++ part1/ch04/ex01.py
@@ -0,0 +1,15 @@
+#데이터 타입
+str1 = 'Niceman'
+str2 = 'Goodbye'
+bool1 = True
+float1 = 10.33
+int1 = 4
+dict1 = {
+    'name': 'kim',
+    'age': 15
+}
+tuple1 = 3, 5, 7
+set1 = {7, 8, 9}
+list1 = [3, 5, 7]
+
+print(type(tuple1))
