d6fd4e6113c9e1161be458d1e40f380e273f2b88
devnewv <devnewv@gmail.com>
2023-03-09 00:33:26 +0900

    datatype

--- part1/ch04/ex01.py
+++ part1/ch04/ex01.py
@@ -1,6 +1,7 @@
 #데이터 타입
+import math
+
 str1 = 'Niceman'
-str2 = 'Goodbye'
 bool1 = True
 float1 = 10.33
 int1 = 4
@@ -12,4 +13,24 @@ tuple1 = 3, 5, 7
 set1 = {7, 8, 9}
 list1 = [3, 5, 7]
 
+print(type(str1))
+print(type(bool1))
+print(type(float1))
+print(type(int1))
+print(type(dict1))
 print(type(tuple1))
+print(type(set1))
+print(type(list1))
+print("=========================================================")
+
+a = 5.
+b = 4
+
+print(type(a), type(b))
+result1 = a + b
+print(result1, type(result1))
+print(int(result1), type(int(result1)))
+print("=========================================================")
+
+print(math.ceil(5.22))
+print(math.floor(3.99))
